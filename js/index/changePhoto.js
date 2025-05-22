a747c253f5db994b279c2e8ef3047220b3751926
Gerosd <gerosdd@gmail.com>
2025-05-22 18:10:19 +0300

    services slider

--- js/index/changePhoto.js
+++ js/index/changePhoto.js
@@ -2,8 +2,8 @@ export function changePhoto(theme) {
     const hero = document.querySelector(".hero");
     if (hero) {
         const imageUrl = theme === 'dark'
-            ? '../images/index/hero-black.jpg'
-            : '../images/index/hero.jpg';
+            ? '/images/index/hero-black.jpg'
+            : '/images/index/hero.jpg';
         hero.style.backgroundImage = `url('${imageUrl}')`;
     } else {
         console.error('Элемент .hero не найден');
